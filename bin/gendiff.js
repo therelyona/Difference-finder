9686cc2d11985dc5a99b80054f9bb6c12ceb5e9a
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-07-28 20:56:05 +0400

    added arguments and .option bin/gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,8 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format')
   .helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
