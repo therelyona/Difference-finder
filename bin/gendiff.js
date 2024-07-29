de9df2f24c75d3044ccb226f9ce129b6f1ef2d14
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-07-29 18:54:18 +0400

    modified gendiff.js, add .action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import parseFile from '../src/parse.js';
 
 program
   .name('gendiff')
@@ -7,6 +8,12 @@ program
   .version('1.0.0', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .action((filepath1, filepath2) => {
+    const file1Data = parseFile(filepath1);
+    const file2Data = parseFile(filepath2);
+    console.log('File 1:', file1Data);
+    console.log('File 2:', file2Data);
+  });
 
 program.parse(process.argv);
