08f202104b7a28dfdace0204b7a67bcfd35b46ca
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-08-01 16:16:22 +0400

    rename parse.js -> parsers.js

--- src/parse.js
+++
@@ -1,9 +0,0 @@
-const getParse = (content, extension) => {
-  if (extension === '.json') {
-    return JSON.parse(content);
-  }
-
-  throw new Error(`Unsupported file format: ${extension}`);
-};
-
-export default getParse;
