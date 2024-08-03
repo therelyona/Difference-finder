8df890d742a4a2ca2513941dfa64d0d31a5acf2b
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-08-03 21:25:26 +0400

    modified src/index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import path from 'path';
 import makeAbsolutePath from './utils/path.js';
 import getParse from './parsers.js';
 import findDifferences from './differences.js';
-import formatDiff from './utils/stylish.js';
+import chooseFormat from './formatters/index.js';
 
-const getDiff = (filePath1, filePath2, format = 'stylish') => {
+const getDiff = (filePath1, filePath2, formatName = 'stylish') => {
   const path1 = makeAbsolutePath(filePath1);
   const path2 = makeAbsolutePath(filePath2);
 
@@ -19,7 +19,7 @@ const getDiff = (filePath1, filePath2, format = 'stylish') => {
   const parsed2 = getParse(content2, extension2);
 
   const differences = findDifferences(parsed1, parsed2);
-  const diff = formatDiff(differences);
+  const diff = chooseFormat(differences, formatName);
 
   return diff;
 };
