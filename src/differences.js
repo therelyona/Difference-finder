90dd4b25d89480ba9f10eaa8f1292e3338932ad6
soboleva.alyona <soboleva.alyonka@gmail.com>
2024-08-07 17:07:02 +0400

    modified differences

--- src/differences.js
+++ src/differences.js
@@ -5,22 +5,25 @@ const findDifferences = (data1, data2) => {
   const sortKeys = _.sortBy(keys);
 
   const differences = sortKeys.map((key) => {
+    if (!_.has(data1, key)) {
+      return { key, value: data2[key], type: 'added' };
+    }
+    if (!_.has(data2, key)) {
+      return { key, value: data1[key], type: 'deleted' };
+    }
+
     const value1 = data1[key];
     const value2 = data2[key];
+
     if (_.isEqual(value1, value2)) {
       return { key, value: value1, type: 'unchanged' };
     }
-    if (!_.has(data1, key) && _.has(data2, key)) {
-      return { key, value: value2, type: 'added' };
-    }
-    if (_.has(data1, key) && !_.has(data2, key)) {
-      return { key, value: value1, type: 'deleted' };
-    }
-    if (_.isObject(value1) && _.isObject(value2)) {
+    if (_.isPlainObject(value1) && _.isPlainObject(value2)) {
       return { key, children: findDifferences(value1, value2), type: 'nested' };
     }
+
     return {
-      key, firstValue: value1, secondValue: value2, type: 'changed',
+      key, value1, value2, type: 'changed',
     };
   });
 
